326d75dba18b90e84d0282e964f6105b9e9560fb
zubayer-salehin <freelancerzubear@gmail.com>
2022-12-31 18:54:26 +0600

    dsf

--- index.js
+++ index.js
@@ -31,6 +31,14 @@ app.get('/', (req, res) => {
 // mongodb url
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bdkak.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect()
+    .then((result) => {
+        console.log("MongoDB Connected");
+    })
+    .catch((error) => {
+        console.log("MongoDB not connected");
+    });
+
 
 // verify JWT token
 function verifyJWT(req, res, next) {
